ab0cf6e243975660b01678303783382d389503de
Jgffyjj <smlosafo@gmail.com>
2024-11-12 15:34:09 +0000

    all initialized files

---
+++ StatsGH/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        EB: ["EB Garamond", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
